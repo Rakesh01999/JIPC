df489d74059b6686d87c3b8e089a63781a643d5f
Rakesh Biswas <rbiswas01999@gmail.com>
2026-04-02 10:38:02 +0600

    practise: four pillars of OOP practiced

---
+++ CS ~ B-21/Class-03 Four Pillars of OOP/practice_four_pillars_OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+public class Student
+{
+    private string name;
+    private int Age;
+    private double Bengali;
+    private double English;
+    private double Math;
+    
+    // public double getBengali()
+    // {
+    //     return Bengali;
+    // }
+    public string Name
+    {
+        get {return name;}
+        set {name = value;}
+    }
+    public void setBengali(double bengali)
+    {
+        if (bengali >= 0 && bengali <= 100)
+        {
+            Bengali = bengali;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Bengali marks. Please enter a value between 0 and 100.");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        student1.Name= "";
+        Console.WriteLine("Student Name: " + student1.Name);
+    }
+}
